4d105a22bc83ffd7782b0832682dd13eadf752a7
josehenriqve <jose8hnrique@gmail.com>
2023-03-17 09:17:49 -0300

    initial commit

---
+++ primeiro/src/main/java/com/primeiro/br/primeiro/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.primeiro.br.primeiro;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
